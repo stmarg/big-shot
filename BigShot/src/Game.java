48ad2bd73b5d741a30b9e50bdab6bcfebfeb6f6d
Kevinland <Kevinland@graces-ipad.smes.org>
2016-10-11 09:44:53 -0700

    Piano because why not

--- BigShot/src/Game.java
+++ BigShot/src/Game.java
@@ -1,10 +1,10 @@
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import tsinn.ui.SimpleApp;
-
-
+import javafx.scene.image.Image;
 
 /**
  * @author Mr. Sinnott
@@ -12,16 +12,18 @@ import tsinn.ui.SimpleApp;
  */
 public class Game extends SimpleApp
 {
-
 	/**
 	 * Sets up the initial state of the game
 	 * @param gc The GraphicsContext that all drawing will be done to. 
 	 */
+	
+	Image piano = new Image("Golden Piano.png");
 	public void setupApp(GraphicsContext gc)
 	{
 		gc.setFont(new Font(50));
 		gc.setFill(Color.MEDIUMVIOLETRED);
 		gc.setTextAlign(TextAlignment.CENTER);
+		
 	}
 	
 	/**
@@ -30,6 +32,7 @@ public class Game extends SimpleApp
 	 */
 	public void draw(GraphicsContext gc)
 	{
+		gc.drawImage(piano, 100, 100);
 		gc.fillText("Hello Everyone! How are we doing?", getWidth()/2, getHeight()/2);
 		gc.fillText("I'm Kevin", getWidth()/2, getHeight()/2 + 100);
 	}
