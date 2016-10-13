9f078eca0c95ec9e7a88744160cf1c9ac19086ec
Kevinland <Kevinland@graces-ipad.smes.org>
2016-10-13 11:47:58 -0700

    DEVIL

--- BigShot/src/Game.java
+++ BigShot/src/Game.java
@@ -13,13 +13,17 @@ import javafx.scene.image.Image;
 public class Game extends SimpleApp {
 	Image piano = new Image("Golden Piano.png");
 	Image d = new Image("Devil.png");
-	int x;
+	
+	private double x;
+	private double y;
+	private double tx;
+	private double ty;
 
 	public void setupApp(GraphicsContext gc) {
 		gc.setFont(new Font(50));
 		gc.setTextAlign(TextAlignment.CENTER);
-		x = 1;
-
+		x = getWidth()/2;
+		y = getHeight()/2;
 	}
 
 	public void draw(GraphicsContext gc) {
@@ -30,15 +34,27 @@ public class Game extends SimpleApp {
 		gc.setFill(Color.LIGHTBLUE);
 		gc.fillOval((getWidth() / 2) - 400, getHeight() / 2 - 100, 800, 200);
 		gc.setFill(Color.MEDIUMVIOLETRED);
-		gc.fillText("This is a coding project. yay", getWidth() / 2, getHeight() / 4);
+		gc.fillText("Click anywhere to move the devil - Kevin", getWidth() / 2, getHeight() / 4);
 		gc.fillText("Hello Everyone! How are we doing?", getWidth() / 2, getHeight() / 2);
 		gc.fillText("I'm Julia & Madi & Kevin!", getWidth() / 2, getHeight() / 2 + 100);
 
-		gc.drawImage(d, x, 0, 100, 100);
-
-		x = (x + 5+getWidth())%getWidth();
+		gc.drawImage(d, x - 50, y - 50, 100, 100);
 		
+		if (tx > x) {
+			x = x + 5;
+		}
+		 
+		if (tx < x) {
+			x = x - 5;
+		}
 		
+		if (ty > y) {
+			y = y + 5;
+		}
+		
+		if (ty < y) {
+			y = y - 5;
+		}
 	}
 
 	public void updateAnimation(long millis) {
