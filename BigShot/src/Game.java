f44528f179edcf30e0cee825bc3c8fcf3be49b0f
Kevinland <Kevinland@graces-ipad.smes.org>
2016-10-13 14:43:05 -0700

    Added Shrek chasing Sonic and rainbow "gotta go faster" text

--- BigShot/src/Game.java
+++ BigShot/src/Game.java
@@ -9,48 +9,67 @@ import javafx.scene.image.Image;
 
 /**
  * @author Mr. Sinnott
- *
  */
 public class Game extends SimpleApp {
-	Image puppy = new Image("memelord.jpg");
-	Image d = new Image("Devil.png");
+	Image bg = new Image("memelord.jpg");
 	Image sanic = new Image("sanic.png");
+	Image s = new Image("Shrek.png");
 	private double x;
 	private double y;
 	private double tx;
 	private double ty;
-	//private double speedX = 0;
-//	private double speedY = 0;
+	private double sx;
+	private double sy;
+	
 	private double deltax;
-   private double deltay;
-    private double angle;
+	private double deltay;
+	private double angle;
+	
+	private double sdeltax;
+	private double sdeltay;
+	private double sangle;
+	
+	private int r;
+	private int g;
+	private int b;
 
 	public void setupApp(GraphicsContext gc) {
 		gc.setFont(new Font(50));
 		gc.setTextAlign(TextAlignment.CENTER);
-		x = getWidth()/2;
-		y = getHeight()/2;
+		x = getWidth() / 2;
+		y = getHeight() / 2;
+		
+		r = 255;
+		g = 140;
+		b = 0;
 	}
 
 	public void draw(GraphicsContext gc) {
-
 		gc.setGlobalAlpha(0.25);
-		gc.drawImage(puppy, 0, 0, getWidth(), getWidth());
+		gc.drawImage(bg, 0, 0, getWidth(), getWidth());
 		gc.setGlobalAlpha(1);
-		//gc.setFill(Color.LIGHTGREEN);
-		//gc.fillRect((getWidth() / 2) - 400, getHeight() / 2 - 100, 800, 200);
+		
 		gc.setFill(Color.BLUE);
-		gc.fillText("Click anywhere to move the devil - Kevin", getWidth() / 2, getHeight() / 4);
+		gc.fillText("Click Anywhere to Move the Sanic - Kevin", getWidth() / 2, getHeight() / 4);
 		gc.fillText("Hello Everyone! How are we doing?", getWidth() / 2, getHeight() / 2);
-		gc.fillText("I'm Julia & Madi & Kevin!", getWidth() / 2, getHeight()*0.66);
-		gc.fillText("gotta go fast", tx,ty);
+		gc.fillText("I'm Julia & Madi & Kevin!", getWidth() / 2, getHeight() * 0.66);
+		
+		//gc.setFill(Color.DARKORANGE);
+		gc.setFill(Color.rgb(r, g, b));
+		gc.fillText("Gotta Go Fast", tx, ty);
+		
 		gc.drawImage(sanic, x - 50, y - 50, 100, 100);
+		gc.drawImage(s, sx - 50, sy - 50, 100, 100);
 		
-		//x = x - Math.cos(angle) * 5;
-	   // y = y - Math.sin(angle) * 5;
-		x=(x+deltax + getWidth()) % getWidth();
-		y=(y+deltay + getHeight()) % getHeight();
+		x = (x + deltax + getWidth()) % getWidth();
+		y = (y + deltay + getHeight()) % getHeight();
 		
+		sx = (sx + sdeltax + getWidth()) % getWidth();
+		sy = (sy + sdeltay + getHeight()) % getHeight();
+		
+		sangle = Math.atan2(y - sy, x - sx);
+		sdeltax = Math.cos(sangle) * 3;
+		sdeltay = Math.sin(sangle) * 3;
 	}
 
 	public void updateAnimation(long millis) {
@@ -59,13 +78,16 @@ public class Game extends SimpleApp {
 	public static void main(String[] args) {
 		launch();
 	}
-	
+
 	public void onMousePressed(MouseEvent me) {
 		tx = me.getX();
 		ty = me.getY();
-	    angle = Math.atan2( ty-y,  tx-x);
-	    deltax =Math.cos(angle) * 5;
-	    deltay =Math.sin(angle) * 5;
-	    
+		angle = Math.atan2(ty - y, tx - x);
+		deltax = Math.cos(angle) * 5;
+		deltay = Math.sin(angle) * 5;
+		
+		r = (int)(Math.random() * 255) + 1;
+		g = (int)(Math.random() * 255) + 1;
+		b = (int)(Math.random() * 255) + 1;
 	}
 }
